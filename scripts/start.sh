a065909f503e46ed0fbe9780c23571333dbcb892
Tim Schaefer <tim.schaefer.de@gmail.com>
2022-03-17 00:18:45 +0100

    user scripts

---
+++ scripts/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker_path=`realpath $(dirname $(readlink -f $0))/../`
+cd $docker_path/bin 
+
+source ../.env
+
+RC=0
+
+sudo bash ./fresh-start.sh --update-nginx-template --use-nginx-conf-files --yes -e $DEFAULT_EMAIL
+
+exit $RC
---
+++ scripts/stop.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+docker_path=`realpath $(dirname $(readlink -f $0))/../`
+cd $docker_path
+RC=0
+/usr/bin/docker-compose down -v --remove-orphans || RC=1
+# rm data/conf.d/default.conf
+exit $RC
